b939b5d52e66e80cf02481cf3b15b34e98d7628e
xsSplater <xssplater@gmail.com>
2023-04-06 10:42:11 +0300

    Crosshairs Fix. Russian localization.
    Also added the possibility of localization of the name.

--- crosshairs_fix/scripts/mods/crosshairs_fix/crosshairs_fix_localization.lua
+++ crosshairs_fix/scripts/mods/crosshairs_fix/crosshairs_fix_localization.lua
@@ -1,5 +1,10 @@
 return {
+	mod_name = {
+		en = "Crosshairs Fix",
+		ru = "Исправление: перекрестия",
+	},
 	mod_description = {
 		en = "Fixes crosshairs to be actually representative of spread, taking your FOV into account.",
+		ru = "Исправление перекрестий, чтобы они отображали фактический разброс с учётом вашего поля зрения.",
 	},
 }
