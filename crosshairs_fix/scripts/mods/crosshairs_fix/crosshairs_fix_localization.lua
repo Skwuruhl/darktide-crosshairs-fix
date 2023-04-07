7a1ecd02cb311036d3bf610e4b45f9ffe4e599fd
xsSplater <xssplater@gmail.com>
2023-04-07 13:15:48 +0300

    Delete crosshairs_fix_localization.lua

--- crosshairs_fix/scripts/mods/crosshairs_fix/crosshairs_fix_localization.lua
+++
@@ -1,9 +0,0 @@
-return {
-	mod_name = {
-		en = "Crosshairs Fix",
-	},
-	mod_description = {
-		en = "Fixes crosshairs to be actually representative of spread, taking your FOV into account.",
-		ru = "Исправление перекрестий, чтобы они отображали фактический разброс с учётом вашего поля зрения.",
-	},
-}
