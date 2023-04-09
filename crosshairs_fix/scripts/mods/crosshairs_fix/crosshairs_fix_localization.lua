8dc6699a21444f64360b9e1167fb1f94c368b016
xsSplater <xssplater@gmail.com>
2023-04-09 12:50:33 +0300

    Update crosshairs_fix_localization.lua

--- crosshairs_fix/scripts/mods/crosshairs_fix/crosshairs_fix_localization.lua
+++ crosshairs_fix/scripts/mods/crosshairs_fix/crosshairs_fix_localization.lua
@@ -5,6 +5,6 @@ return {
 	},
 	mod_description = {
 		en = "Fixes crosshairs to be actually representative of spread, taking your FOV into account.",
-		ru = "Исправление перекрестий, чтобы они отображали фактический разброс с учётом вашего поля зрения.",
+		ru = "Crosshairs Fix - Исправление перекрестий, чтобы они отображали фактический разброс с учётом вашего поля зрения.",
 	}, 
 }
