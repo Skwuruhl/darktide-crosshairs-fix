3ed2cfac1bb8d9a5416619675580103e265d8a0f
Skwuruhl <smmarion11@gmail.com>
2024-06-28 12:34:29 -0700

    separated example crosshair

--- crosshairs_fix/scripts/mods/crosshairs_fix/crosshairs_fix.lua
+++ crosshairs_fix/scripts/mods/crosshairs_fix/crosshairs_fix.lua
@@ -26,6 +26,8 @@ mod:hook(fov, "apply_fov_to_crosshair", function(func, pitch, yaw)
 	return pitch, yaw
 end)
 
+--assault is the only template that has a multiplier of 15 instead of 10.
+--I tried to fix this without hook_origin but I couldn't get it to work. Only thing this hook changes is SPREAD_DISTANCE 15 to 10
 mod:hook_origin(assault_new, "update_function", function(parent, ui_renderer, widget, template, crosshair_settings, dt, t, draw_hit_indicator)
 	local style = widget.style
 	local hit_progress, hit_color, hit_weakspot = parent:hit_indicator()
@@ -36,14 +38,12 @@ mod:hook_origin(assault_new, "update_function", function(parent, ui_renderer, wi
 		local scalar = SPREAD_DISTANCE * (crosshair_settings.spread_scalar or 1)
 		local spread_offset_y = pitch * scalar
 		local spread_offset_x = yaw * scalar
-		local x, y
 
 		local top_style = style.top
 		local top_size = top_style.size[1]
-		x, y = mod.idk_what_to_call_this(spread_offset_x, spread_offset_y, top_style.angle, top_size)
 
-		top_style.offset[1] = x
-		top_style.offset[2] = y
+		top_style.offset[1] = 0
+		top_style.offset[2] = math.min(-spread_offset_y - top_size/2, -top_size/2-2)
 
 		local bottom_style = style.bottom
 		local bottom_size = bottom_style.size[1]
@@ -66,44 +66,3 @@ mod:hook_origin(assault_new, "update_function", function(parent, ui_renderer, wi
 
 	Crosshair.update_hit_indicator(style, hit_progress, hit_color, hit_weakspot, draw_hit_indicator)
 end)
-
---assault is the only template that has a multiplier of 15 instead of 10.
---I tried to fix this without hook_origin but I couldn't get it to work. Only thing this hook changes is SPREAD_DISTANCE 15 to 10
--- mod:hook_origin(assault_new, "update_function", function(parent, ui_renderer, widget, template, crosshair_settings, dt, t, draw_hit_indicator)
--- 	local style = widget.style
--- 	local hit_progress, hit_color, hit_weakspot = parent:hit_indicator()
--- 	local yaw, pitch = parent:_spread_yaw_pitch(dt)
--- 	local SPREAD_DISTANCE = 10
-
--- 	if yaw and pitch then
--- 		local scalar = SPREAD_DISTANCE * (crosshair_settings.spread_scalar or 1)
--- 		local spread_offset_y = pitch * scalar
--- 		local spread_offset_x = yaw * scalar
-
--- 		local top_style = style.top
--- 		local top_size = top_style.size[1]
-
--- 		top_style.offset[1] = 0
--- 		top_style.offset[2] = math.min(-spread_offset_y - top_size/2, -top_size/2-2)
-
--- 		local bottom_style = style.bottom
--- 		local bottom_size = bottom_style.size[1]
-
--- 		bottom_style.offset[1] = 0
--- 		bottom_style.offset[2] = math.max(spread_offset_y + bottom_size/2, bottom_size/2+2)
-
--- 		local left_style = style.left
--- 		local left_size = left_style.size[1]
-
--- 		left_style.offset[1] = math.min(-spread_offset_x - left_size/2, -left_size/2-2)
--- 		left_style.offset[2] = 0
-
--- 		local right_style = style.right
--- 		local right_size = right_style.size[1]
-
--- 		right_style.offset[1] = math.max(spread_offset_x + right_size/2, right_size/2+2)
--- 		right_style.offset[2] = 0
--- 	end
-
--- 	Crosshair.update_hit_indicator(style, hit_progress, hit_color, hit_weakspot, draw_hit_indicator)
--- end)
