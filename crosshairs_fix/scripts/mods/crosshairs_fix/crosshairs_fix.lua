cd55c2d8d614bf30147d25b3a6fdee5e5651b15f
Skwuruhl <smmarion11@gmail.com>
2025-07-03 20:55:15 -0700

    fixed crosshair_rotation for non-90° crosshairs

--- crosshairs_fix/scripts/mods/crosshairs_fix/crosshairs_fix.lua
+++ crosshairs_fix/scripts/mods/crosshairs_fix/crosshairs_fix.lua
@@ -41,12 +41,12 @@ end
 --minimum_offset is the mininum number of 1080 pixels the returned x, y should be from center. e.g. a value of 1 at an angle of 45° would set a minumum x and y value of 0.707. optional. Don't forget to include half crosshair here as well.
 --texture_rotation is an optional parameter in case the crosshair texture needs additional rotation. e.g. If you add 90 deg to _crosshair_segment() to rotate the texture, then pass 90 deg to texture rotation so it undoes the rotation for the purposes of crosshair placement
 --As usual for lua all angles should be supplied in radians.
---0° is left and then rotates clockwise. Based on vanilla crosshair segment values.
+--angle is per a unit circle. e.g. 0° is the right side and rotation is counter clockwise.
 mod.crosshair_rotation = function(x, y, angle, half_crosshair_size, minimum_offset, texture_rotation)
 	minimum_offset = minimum_offset or 0
 	texture_rotation = texture_rotation or 0
-	x = -math.cos(angle - texture_rotation) * math.max(x + half_crosshair_size, minimum_offset)
-	y = math.sin(angle - texture_rotation) * math.max(y + half_crosshair_size, minimum_offset)
+	x = math.cos(angle - texture_rotation) * math.max(x + half_crosshair_size, minimum_offset)
+	y = -math.sin(angle - texture_rotation) * math.max(y + half_crosshair_size, minimum_offset)
 	return x, y
 end
 
