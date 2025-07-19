33bc12792a97843c2b7286611131a64a559fb5f1
xsSplater - СанСаныч <xssplater@gmail.com>
2025-07-19 05:33:43 +0300

    Update RU crosshairs_fix_localization.lua

--- crosshairs_fix/scripts/mods/crosshairs_fix/crosshairs_fix_localization.lua
+++ crosshairs_fix/scripts/mods/crosshairs_fix/crosshairs_fix_localization.lua
@@ -1,29 +1,28 @@
 return {
 	mod_name = {
 		en = "Crosshairs Fix",
-		ru = "Исправление: перекрестия",
+		ru = "Исправление: прицелы",
 		["zh-cn"] = "准星修复",
 	},
 	mod_description = {
 		en = "Fixes crosshairs to be actually representative of spread, taking your FOV into account.",
-		ru = "Crosshairs Fix - Исправление перекрестий, чтобы они отображали фактический разброс с учётом вашего поля зрения.",
-		["zh-cn"] = "修复准星，在考虑 FOV 因素的情况下，使其准确反映武器散布范围。",
-	},
-	mod_description = {
-		en = "Fixes crosshairs to be actually representative of spread, taking your FOV into account.",
-		ru = "Crosshairs Fix - Исправление перекрестий, чтобы они отображали фактический разброс с учётом вашего поля зрения.",
+		ru = "Crosshairs Fix - Исправление для прицелов, которое показывает фактический разброс с учётом вашего поля зрения.",
 		["zh-cn"] = "修复准星，在考虑 FOV 因素的情况下，使其准确反映武器散布范围。",
 	},
 	shotshell_spread_crosshair_center_title = {
 		en = "Center Shotshell Spread Crosshair Alignment",
+		ru = "Выравнивание перекрестья по центру зоны разброса дроби",
 	},
 	shotshell_spread_crosshair_center_tooltip = {
 		en = "Toggles whether spread crosshairs are aligned to the center or perimeter of the shotshell.",
+		ru = "Переключает выравнивание расширения прицелов относительно разброса: по центру или по периметру зоны разброса дроби.",
 	},
 	shotshell_spread_crosshair_disable_title = {
 		en = "Disable Spread Crosshairs",
+		ru = "Отключить разброс прицелов",
 	},
 	shotshell_spread_crosshair_disable_tooltip = {
 		en = "Spread will instead be added to the shotshell crosshairs.",
+		ru = "Разброс теперь будет добавляться к прицелам по зоне разброса дроби.",
 	}
 }
